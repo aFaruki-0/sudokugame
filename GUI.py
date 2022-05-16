538be5c90eca46ed46cbfdb87630fd45c6f052a8
Ömer Faruk Sevük <o.farukrt@gmail.com>
2022-05-16 20:58:34 +0300

    replace time and strikes

--- GUI.py
+++ GUI.py
@@ -132,10 +132,10 @@ def redraw_window(win, board, time, strikes):
     # Draw time
     fnt = pygame.font.SysFont("comicsans", 40)
     text = fnt.render("Time: " + format_time(time), 1, (0,0,0))
-    win.blit(text, (540 - 160, 560))
+    win.blit(text, (540 - 220, 540))
     # Draw Strikes
     text = fnt.render("X " * strikes, 1, (255, 0, 0))
-    win.blit(text, (20, 560))
+    win.blit(text, (20, 540))
     # Draw grid and board
     board.draw(win)
 
