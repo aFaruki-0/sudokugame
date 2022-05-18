199397619d8361455905b23766261a0c77f9582a
Ömer Faruk Sevük <o.farukrt@gmail.com>
2022-05-18 22:33:59 +0300

    adding board

--- GUI.py
+++ GUI.py
@@ -1,6 +1,9 @@
 import pygame
 import time
+import requests
 
+response = requests.get("https://sugoku.herokuapp.com/board?difficulty=easy")
+grid = response.json()['board']
 pygame.init()
 Width = 540
 Height = 580
@@ -8,7 +11,7 @@ title = "Sudoku Game"
 color = (239, 234, 216)
 running = True
 start = time.time()
-
+numsPos = []
 myfont = pygame.font.SysFont('Comic Sans MS', 35)
 window = pygame.display.set_mode((Width, Height))
 pygame.display.set_caption(title)
@@ -22,16 +25,33 @@ def drawTimer(gameTime, font):
 def drawKey(font):
     showKey = font.render(f"Key: {key}", True, (0,0,0))
     window.blit(showKey, (50, 500))
+def decideNumsPos(numsPos):
+    for i in range(9):
+        for a in range(9):
+            numsPos.append([48*(i+1), 48*(a+1)])
 def board():
     for i in range(9):
         for a in range(9):
             pygame.draw.rect(window, (0,0,0),[48*(i+1), 48*(a+1), 50, 50],2)
-def redraw_window(gameTime, font):
+
+def drawNums(grid, numsPos, font):
+    a=-1
+    for b in range(9):
+            for i in grid[b]:
+                if i == 0 :
+                    a +=1
+                    continue
+                else:
+                    a +=1
+                    num = font.render(f"{i}",True,(0,0,0))
+                    window.blit(num, (numsPos[a][0], numsPos[a][1]))
+def redraw_window(gameTime, font, numsPos, grid):
     window.fill(color)
     board()
+    decideNumsPos(numsPos)
     drawKey(font)
     drawTimer(gameTime, font)
-
+    drawNums(grid, numsPos, font)
 while running:
     gameTime = round(time.time()-start)
 
@@ -39,8 +59,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if mouse[0] > 50 and mouse[0] < 100 and mouse[1] > 50 and mouse[1] < 100:
-                running= False
+            pass
         if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_1:
                     key = 1
@@ -62,6 +81,7 @@ while running:
                     key = 9
                 if event.key == pygame.K_ESCAPE:
                     key=None
+
     mouse = pygame.mouse.get_pos()
-    redraw_window(gameTime, myfont)
+    redraw_window(gameTime, myfont, numsPos, grid)
     pygame.display.update()
