4fb086daaedb5100f9ddd9b1f26e11ea63cd3fd2
Ömer Faruk Sevük <o.farukrt@gmail.com>
2022-07-08 11:00:40 +0300

    solver fix

--- solver.py
+++ solver.py
@@ -3,14 +3,7 @@ import numpy as np
 response = requests.get("https://sugoku.herokuapp.com/board?difficulty=easy")
 grid=np.array(response.json()['board'])
 empties = grid==0
-solved=grid.copy()
 for i in range(9):
     for j in range(9):
         if empties[i,j]==True:
-            for a in range(9):
-                if a+1==grid[i,a] and a+1==grid[a,j]:
-                    continue
-                else:
-                    solved[i,j]=a+1
-print(grid)
-print(solved)
+            pass
