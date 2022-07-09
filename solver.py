87402041d498d3549d8f31f6fcb2cb0b040cd402
Ömer Faruk Sevük <o.farukrt@gmail.com>
2022-07-09 07:21:35 +0300

    solver :)

--- solver.py
+++ solver.py
@@ -1,9 +1,47 @@
 import requests
 import numpy as np
 response = requests.get("https://sugoku.herokuapp.com/board?difficulty=easy")
-grid=np.array(response.json()['board'])
-empties = grid==0
-for i in range(9):
-    for j in range(9):
-        if empties[i,j]==True:
-            pass
+board=np.array(response.json()['board'])
+def solve(board):
+    findEmpty = emptyCell(board)
+    
+    if not findEmpty:
+        return True
+    else:
+        row, column = findEmpty
+    
+    for i in range(1,10):
+        if isValid(board, i, (row, column)):
+            board[row][column] = i
+            if solve(board):
+                return True
+
+        board[row][column] = 0
+    return False
+             
+
+def isValid (board, num, pos):
+    for i in range(9):
+        if num == board[pos[0]][i]:
+            return False
+    for i in range(9):
+        if num == board[i][pos[1]]:
+            return False
+
+    row = pos[0] // 3 
+    column = pos[1] // 3 
+
+    for i in range(row * 3, (row * 3) + 3):
+        for j in range(column * 3, (column * 3) + 3):
+            if num == board[i][j]:
+                return False 
+    return True
+
+def emptyCell(board):
+    for row in range(9):
+        for column in range(9):
+            if board[row][column] == 0:
+                return (row,column)
+    return None
+solve(board)
+print(board)
